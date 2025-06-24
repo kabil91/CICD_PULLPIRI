bbf924ff349dfd1fe42e7d51a233596b254180c7
akshaylg0314 <akshay14.garg@lge.com>
2025-06-24 11:49:48 +0530

    Delete pullpiri-refactoring directory

--- pullpiri-refactoring/src/server/apiserver/src/grpc/sender/mod.rs
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: Copyright 2024 LG Electronics Inc.
- * SPDX-License-Identifier: Apache-2.0
- */
-
-//! Running gRPC message sending
-
-pub mod filtergateway;
-pub mod nodeagent;
--- pullpiri-refactoring/src/server/apiserver/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod artifact;
-pub mod grpc;
-pub mod manager;
-pub mod route;
