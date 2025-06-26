64db45cd76d3a8f53e5e7cf7a2330d4f057b1658
akshay14.garg <akshay14.garg@lge.com>
2025-06-26 15:44:23 +0530

    new .yml

--- scripts/installdeps.sh
+++ scripts/installdeps.sh
@@ -16,6 +16,7 @@ common_packages=(
   curl
   libssl-dev
   nodejs
+  npm
 )
 DEBIAN_FRONTEND=noninteractive apt-get install -y "${common_packages[@]}"
 echo "✅ Base packages installed successfully."
